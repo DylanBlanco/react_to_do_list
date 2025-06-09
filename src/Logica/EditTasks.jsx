e855c4381e2b19535d29346daca55f84356d70cd
DylanBlanco <dylanblanco1198@gmail.com>
2025-06-09 20:01:59 +0200

    creato template

--- src/Logica/EditTasks.jsx
+++ src/Logica/EditTasks.jsx
@@ -1,11 +1,29 @@
 function EditTasks () {
+    // Logica  ------------------------------------------------
+    
+
+
+    // Template  ----------------------------------------------
     return (
-        <>
-            <div className="EditTasks">
-                
+        <div className="edit-task mt-2">
+            <input
+                type="text"
+                className="form-control mb-2"
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+            />
+            <div className="d-flex gap-3">
+
+                <button className="btn btn-success" onClick={handleSave}>
+                    Salva
+                </button>
+
+                <button className="btn btn-secondary" onClick={onClose}>
+                    Annulla
+                </button>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default EditTasks
