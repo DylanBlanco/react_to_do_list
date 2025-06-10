511cff31d4e9274cfa6d957f9f936b1613a64e98
DylanBlanco <dylanblanco1198@gmail.com>
2025-06-10 18:28:16 +0200

    DeleteTasks.jsx - function handleTasks

--- src/Logica/DeleteTasks.jsx
+++ src/Logica/DeleteTasks.jsx
@@ -1,11 +1,17 @@
 export default function DeleteTasks ({index, tasks, setTask}) {
     // Logica -------------------------------------
+    const handleTasks = () => {
+        const updateTasks = tasks.filter((_, i) => i !== index);
+        setTask(updateTasks);
+    };
+
     
     // Template -----------------------------------
     return (
         <>
             <button
                 className="btn btn-danger d-flex align-items-center"
+                onClick={handleTasks}
             >
                 <img
                     src="/public/Img/trash.svg"
